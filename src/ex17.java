576fd30766048263b90b563b2f7c074022058647
Mehran <36104315+Mehran-at@users.noreply.github.com>
2018-11-15 01:01:49 +0100

    Delete ex17.java

--- src/ex17.java
+++
@@ -1,8 +0,0 @@
-public class ex17 {
-    public static void main(String[] args) {
-        String str = new String("I luv humbergers.");
-        String jason = new String(" BOO");
-        System.out.print(str);
-        System.out.print(jason);
-    }
-}
